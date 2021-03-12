f176db60cacfeec2c77a848e6bab24598a2e298b
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2021-03-11 20:34:30 -0500

    fixed the platforms
    I arranged my platforms in a way that makes winning a possibility unlike before.

--- Assets/PlayerJump.cs
+++ Assets/PlayerJump.cs
@@ -6,7 +6,7 @@ public class PlayerJump : MonoBehaviour
 {
 
     public float jumpForce = 300.0f;
-    private void FixedUpdate()
+   void FixedUpdate()
     {
         bool jump = Input.GetButtonDown ("Jump");
         if (jump) GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
