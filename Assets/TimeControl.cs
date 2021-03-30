716399ef7b2a9aa7163a71364f642c8bfdc0f44a
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2021-03-30 16:11:33 -0400

    Trying to add a new timer
    I added a new script for a timer with Minutes + seconds

---
+++ Assets/TimeControl.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+public class TimeControl : MonoBehaviour
+{
+    public Text timerText;
+    public float timeLeftSeconds = 240F;
+    public static bool TimesUp = false;
+    public string GameName;
+
+    void Start()
+    {
+        TimesUp = false;
+    }
+
+
+    void Update()
+    {
+        float t = timeLeftSeconds -= Time.deltaTime;
+        string minutes = ((int)t / 60).ToString("00");
+        string seconds = (t % 60).ToString("00");
+        string milliseconds = ((int)(t * 100f) % 100).ToString("00");
+
+        timerText.text = minutes + ":" + milliseconds;
+
+        if (timeLeftSeconds <= 0)
+        {
+            timeLeftSeconds = 0;
+            timerText.text = minutes + ":" + seconds + milliseconds;
+            TimesUp = true;
+        }
+
+        if (TimesUp == true && Input.GetKeyDown("p"))
+            SceneManager.LoadScene(GameName, LoadSceneMode.Single);
+
+    }
+    private void OnGUI()
+    {
+        if (TimesUp == true)
+            GUI.Box(new Rect(0, 50, 250, 25), "GAME OVER. Press 'p' to play again");
+    }
+}
