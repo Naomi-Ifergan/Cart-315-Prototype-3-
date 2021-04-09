073cd3c8cb6aec582f3a95e04dbdaa93a0235451
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2021-04-09 12:47:34 -0400

    more fixes

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,6 +8,8 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+   
+
     public void QuitGame()
     {
         Debug.Log("QUIT");
