d177804d81931c6957b597e47b58b60ae78b9303
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2021-04-09 22:34:47 -0400

    fixing the instruction button

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,7 +4,41 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject instructions;
+    public GameObject mainScreen;
+
+
+    void Start()
+
+    {
+
+        instructions.SetActive(false);
+
+        mainScreen.SetActive(true);
+
+    }
+
+    public void ShowInstructions()
+
+    {
+
+        instructions.SetActive(true);
+
+        mainScreen.SetActive(false);
+
+    }
+    public void HideInstructions()
+
+    {
+
+        instructions.SetActive(false);
+
+        mainScreen.SetActive(true);
+
+    }
+
     public void PlayGame()
+
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -16,3 +50,13 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 }
+
+
+
+
+
+
+
+
+
+
